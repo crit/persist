d9276b1b1643b1c7bd9cf249bcfb2f86f4dd1f40
Crit Russell <critrussell@gmail.com>
2022-10-10 22:37:58 -0600

    embed mysql lib

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/crit/persist
 go 1.19
 
 require github.com/crit/errors v1.0.0
+
+require github.com/go-sql-driver/mysql v1.6.0
--- mysql/mysql.go
+++ mysql/mysql.go
@@ -1,17 +1,24 @@
 package mysql
 
-import "database/sql"
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/crit/errors"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 var pool *sql.DB
 
-func Register(instance *sql.DB) {
+func Register(instance *sql.DB) error {
 	if instance == nil {
-		panic("provided sql.DB instance was nil")
+		return errors.New(http.StatusInternalServerError, "provided sql.DB instance was nil")
 	}
 
 	if pool != nil {
-		panic("attempted second registration of sql.DB instance")
+		return errors.New(http.StatusInternalServerError, "attempted second registration of sql.DB instance")
 	}
 
 	pool = instance
+	return nil
 }
